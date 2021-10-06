5a08e9fcc126f4a139d210542b0bcab381f98ac9
sigristarisa <arisasigrist.ch@gmail.com>
2021-10-06 13:33:00 +0200

    initial commit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const pomodoroTimer = ((flowMin, restMin) => {
   const minute = document.getElementById("minute");
   const second = document.getElementById("second");
   let startButton = document.getElementById("startButton");
-  startButton.classList.add("flow");
+  startButton.className = "flow";
   const title = document.getElementById("title");
   let flowStart = true;
   let restStart = true;
@@ -18,51 +18,54 @@ const pomodoroTimer = ((flowMin, restMin) => {
   second.innerText = String(flowSec % 60).padStart(2, 0);
 
   // ------------------------- FLOW ------------------------- //
+  if (startButton.className === "flow") {
+    startButton.addEventListener("click", () => {
+      // change the background color when the timer starts
+      document.body.style.backgroundColor = "#095809";
 
-  startButton.addEventListener("click", () => {
-    // change the background color when the timer starts
-    document.body.style.backgroundColor = "#095809";
+      // start the flow timer
+      if (flowStart) {
+        // set the interval to count down
+        flowTimer = setInterval(() => {
+          if (flowSec > 0) {
+            flowSec--;
+            minute.innerText = String((flowSec - (flowSec % 60)) / 60).padStart(
+              2,
+              0
+            );
+            second.innerText = String(flowSec % 60).padStart(2, 0);
+          } else if (flowSec === 0) {
+            flowCount++;
+            clearInterval(flowTimer);
+            startButton.className = "rest";
+          }
+        }, 100);
 
-    // start the flow timer
-    if (flowStart) {
-      // set the interval to count down
-      flowTimer = setInterval(() => {
-        if (flowSec > 0) {
-          flowSec--;
-          minute.innerText = String((flowSec - (flowSec % 60)) / 60).padStart(
-            2,
-            0
-          );
-          second.innerText = String(flowSec % 60).padStart(2, 0);
-        } else if (flowSec === 0) {
-          flowCount++;
-          clearInterval(flowTimer);
-          startButton.classList.remove("flow");
-          startButton.classList.add("rest");
-          console.log(startButton.className);
-        }
-      }, 100);
-
-      // change the button color and its text
-      startButton.innerText = "Pause";
-      startButton.style.backgroundColor = "#d11c1c";
-      startButton.style.color = "#f8c464";
-      // change the button color when hovered
-      startButton.onmouseover = () => {
-        startButton.style.color = "#d11c1c";
-        startButton.style.backgroundColor = "#f8c464";
-      };
-      startButton.onmouseout = () => {
-        startButton.style.color = "#f8c464";
+        // change the button color and its text
+        startButton.innerText = "Pause";
         startButton.style.backgroundColor = "#d11c1c";
-      };
-      flowStart = !flowStart;
-    }
-    // pause the timer
-    else {
-      clearInterval(flowTimer);
-      startButton.innerText = "Continue";
-      flowStart = !flowStart;
-    }
-  });
+        startButton.style.color = "#f8c464";
+        // change the button color when hovered
+        startButton.onmouseover = () => {
+          startButton.style.color = "#d11c1c";
+          startButton.style.backgroundColor = "#f8c464";
+        };
+        startButton.onmouseout = () => {
+          startButton.style.color = "#f8c464";
+          startButton.style.backgroundColor = "#d11c1c";
+        };
+        flowStart = !flowStart;
+      }
+      // pause the timer
+      else {
+        clearInterval(flowTimer);
+        startButton.innerText = "Continue";
+        flowStart = !flowStart;
+      }
+    });
+  } else if (startButton.className === "rest") {
+    startButton.addEventListener("click", () => {
+      console.log("hi from the rest");
+    });
+  }
 })(0.25, 0.25);
